5898e4d0311b47139856797bbd0294efa6ce0fee
Fernando Cerini <fernando@soflex.com.ar>
2020-08-07 18:37:12 -0300

    refactory

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -10,27 +10,27 @@ import android.content.pm.PackageManager
 import android.os.Bundle
 import android.os.Handler
 import android.os.ParcelUuid
-import android.util.Log
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlin.math.absoluteValue
 import kotlin.random.Random
 
 
 class MainActivity : AppCompatActivity() {
     private lateinit var btAdapter: BluetoothAdapter
     private lateinit var btLeAdvertiser: BluetoothLeAdvertiser
-
-    private val requestBT = 1234
-    var testData = "000000000000009"
-
+    private val requestBT = 986855
+    var btData = "000000000000009"
     private var btLeScanner = BluetoothAdapter.getDefaultAdapter().bluetoothLeScanner
-    private var mScanning = false
-    private val handler = Handler()
-    private val SCAN_PERIOD: Long = 30000
-    private val leScanCallback: ScanCallback = object : ScanCallback() {
+    private var btScanning = false
+    private val btHandlerStopScan = Handler()
+    private val BT_SCAN_PERIOD: Long = 30000
+    // IMEI, rssi (db)
+    private var btNearbyMap = mutableMapOf<String, Int>()
+    private val btScanCallback: ScanCallback = object : ScanCallback() {
         override fun onScanResult(callbackType: Int, result: ScanResult) {
             super.onScanResult(callbackType, result)
             if (result.scanRecord == null){
@@ -42,14 +42,34 @@ class MainActivity : AppCompatActivity() {
             if (data.size == 0){
                 return
             }
+            var deviceImei = String(data.values.first())
+            var deviceRSSI = result.rssi.absoluteValue
 
-            tView.setText ( tView.text.toString() +
-                    result.rssi.toString() + "db, "+
-                    String(data.values.first()) +"\n")
+            if (deviceImei.startsWith("im:")){
+                deviceImei = deviceImei.removePrefix("im:")
+            } else {
+                return
+            }
+
+            var oldRSSI = btNearbyMap.get(deviceImei)
+            if (oldRSSI == null) {
+                oldRSSI = 999
+            }
+
+            if (deviceRSSI < oldRSSI){
+                btNearbyMap.put(deviceImei, deviceRSSI)
+            }
+
+            tView.setText ("")
+            for (item in btNearbyMap) {
+                tView.setText ( tView.text.toString() +
+                        item.key + ": " +
+                        item.value.toString() +"db \n")
+            }
+
         }
     }
-
-    private val advertiseCallback : AdvertiseCallback = object: AdvertiseCallback(){
+    private val btAdvertiseCallback : AdvertiseCallback = object: AdvertiseCallback(){
         override fun onStartFailure(errorCode: Int) {
             super.onStartFailure(errorCode)
             tView.setText ( "Error: " + errorCode.toString() )
@@ -65,66 +85,66 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        testData = "im:0000000000000" + Random(System.nanoTime()).nextInt(10, 90).toString()
-        btstartAdvertising.setText("Advertise " + testData)
+        btData = "im:0000000000000" + Random(System.nanoTime()).nextInt(10, 90).toString()
+        btstartAdvertising.setText("Advertise " + btData)
 
-        init()
+        btInit()
 
     }
 
-    fun startAdvertising ( v:View){
-        if (!init()){
+    fun btStartAdvertising (v:View){
+        if (!btInit()){
             return
         }
 
-        val settings = buildAdvertiseSettings()
-        val data = buildAdvertiseData()
+        val settings = btBuildAdvertiseSettings()
+        val data = btBuildAdvertiseData()
 
         tView.setText ( "startAdvertising... \n")
 
         btLeAdvertiser.startAdvertising(
             settings, data,
-            advertiseCallback
+            btAdvertiseCallback
         )
 
     }
 
-    fun stopAdvertising ( v:View) {
-        if (!init()){
+    fun btStopAdvertising (v:View) {
+        if (!btInit()){
             return
         }
 
         tView.setText ( "stopAdvertising! \n")
-        btLeAdvertiser.stopAdvertising(advertiseCallback)
-
+        btLeAdvertiser.stopAdvertising(btAdvertiseCallback)
     }
-        fun startScan( v:View){
-            try {
-                if (!init()){
-                    return
-                }
-                if (!mScanning) { // Stops scanning after a pre-defined scan period.
-                    handler.postDelayed({
-                        mScanning = false
-                        btLeScanner.stopScan(leScanCallback)
-                        tView.setText ( tView.text.toString() +  "stopScan \n")
-                    }, SCAN_PERIOD)
-                    mScanning = true
-                    btLeScanner.startScan(leScanCallback)
-                    tView.setText ( "startScan " + SCAN_PERIOD.toString() + "sec. \n")
-                } else {
-                    mScanning = false
-                    btLeScanner.stopScan(leScanCallback)
-                    tView.setText ( "stopScan!")
-                }
 
-            } catch (e: Throwable) {
-                tView.setText (e.message)
+    fun btStartScan(v:View){
+        try {
+            if (!btInit()){
+                return
+            }
+            if (!btScanning) { // Stops scanning after a pre-defined scan period.
+                btHandlerStopScan.postDelayed({
+                    btScanning = false
+                    btLeScanner.stopScan(btScanCallback)
+                    tView.setText ( tView.text.toString() +  "stopScan \n")
+                }, BT_SCAN_PERIOD)
+                btScanning = true
+                btLeScanner.startScan(btScanCallback)
+                tView.setText ( "startScan " + BT_SCAN_PERIOD.toString() + "sec. \n")
+            } else {
+                btScanning = false
+                btLeScanner.stopScan(btScanCallback)
+                tView.setText ( "stopScan!")
             }
 
+        } catch (e: Throwable) {
+            tView.setText (e.message)
+        }
+
     }
 
-    fun init() :Boolean{
+    fun btInit() :Boolean{
         try {
             if (!packageManager.hasSystemFeature(PackageManager.FEATURE_BLUETOOTH_LE)) {
                 tView.setText ("Bluetooth Low Energy not supported! sorry...")
@@ -167,7 +187,7 @@ class MainActivity : AppCompatActivity() {
     }
 
 
-    private fun buildAdvertiseData(): AdvertiseData {
+    private fun btBuildAdvertiseData(): AdvertiseData {
         /**
          * Note: There is a strict limit of 31 Bytes on packets sent over BLE Advertisements.
          * This includes everything put into AdvertiseData including UUIDs, device info, &
@@ -182,7 +202,7 @@ class MainActivity : AppCompatActivity() {
         val dataBuilder = AdvertiseData.Builder()
         dataBuilder.addServiceUuid(service_UUID)
         dataBuilder.setIncludeDeviceName(false)
-        dataBuilder.addServiceData(service_UUID, testData.toByteArray())
+        dataBuilder.addServiceData(service_UUID, btData.toByteArray())
 
         return dataBuilder.build()
     }
@@ -191,7 +211,7 @@ class MainActivity : AppCompatActivity() {
      * Returns an AdvertiseSettings object set to use low power (to help preserve battery life)
      * and disable the built-in timeout since this code uses its own timeout runnable.
      */
-    private fun buildAdvertiseSettings(): AdvertiseSettings? {
+    private fun btBuildAdvertiseSettings(): AdvertiseSettings? {
         val settingsBuilder = AdvertiseSettings.Builder()
         settingsBuilder.setAdvertiseMode(AdvertiseSettings.ADVERTISE_MODE_LOW_LATENCY)
         settingsBuilder.setTimeout(0)
